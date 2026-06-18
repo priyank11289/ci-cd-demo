8acb94749966010cc553dd0dc03b3fb222a48730
Priyank <priyankshri125@email.com>
2026-06-18 11:16:57 +0900

    Format unit tests

--- test/test_math.cpp
+++ test/test_math.cpp
@@ -2,17 +2,8 @@
 
 #include "math_utils.h"
 
-TEST(AddTest, PositiveNumbers)
-{
-    EXPECT_EQ(add(2, 3), 5);
-}
+TEST(AddTest, PositiveNumbers) { EXPECT_EQ(add(2, 3), 5); }
 
-TEST(AddTest, NegativeNumbers)
-{
-    EXPECT_EQ(add(-2, -3), -5);
-}
+TEST(AddTest, NegativeNumbers) { EXPECT_EQ(add(-2, -3), -5); }
 
-TEST(AddTest, MixedNumbers)
-{
-    EXPECT_EQ(add(-2, 3), 1);
-}
+TEST(AddTest, MixedNumbers) { EXPECT_EQ(add(-2, 3), 1); }
